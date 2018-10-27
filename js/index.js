173ba6dc5f19a552beb2b0cd49093b247c884514
Steve Unixson <unixson@gmail.com>
2018-10-27 22:28:37 +0300

    calculator

--- js/index.js
+++ js/index.js
@@ -1,160 +1,293 @@
-const weekSub = [0, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1, 1, 0.9, 0.9, 0.8, 0.8, 0.8, 0.3, 0.2];
-const monthSub = [1.5, 1.4, 1.3, 1.2, 1.1, 1, 0.9, 0.9, 0.8, 0.8, 0.8, 0.8, 0.3, 0.2];
-const oneTimeSub = [1.4, 1.3, 1.2, 1, 1, 0.9, 0.8, 0.7, 0.6, 0.6, 0.6, 0.6, 0.6, 0.3, 0.2];
-
-// const rawDate = new Date();
-// const month = rawDate.getMonth();
-// const year = rawDate.getFullYear();
-function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-}
 
 function getCostWeek(cost) {
-    if (cost <= 1000 && cost <= 2999) {
-        return weekSub[1]
+    if (cost <= 1000 || cost <= 2999) {
+        return 1.7
     }
-    if (cost <= 3000 && cost <= 4999) {
-        return weekSub[2]
+    if (cost <= 3000 || cost <= 4999) {
+        return 1.6
     }
-    if (cost <= 5000 && cost <= 9999) {
-        return weekSub[3]
+    if (cost <= 5000 || cost <= 9999) {
+        return 1.5
     }
-    if (cost <= 10000 && cost <= 19999) {
-        return weekSub[4]
+    if (cost <= 10000 || cost <= 19999) {
+        return 1.4
     }
-    if (cost <= 20000 && cost <= 29999) {
-        return weekSub[5]
+    if (cost <= 20000 || cost <= 29999) {
+        return 1.3
     }
-    if (cost <= 30000 && cost <= 39999) {
-        return weekSub[6]
+    if (cost <= 30000 || cost <= 39999) {
+        return 1.2
     }
-    if (cost <= 40000 && cost <= 49999) {
-        return weekSub[7]
+    if (cost <= 40000 || cost <= 49999) {
+        return 1.1
     }
-    if (cost <= 50000 && cost <= 59999) {
-        return weekSub[8]
+    if (cost <= 50000 || cost <= 59999) {
+        return 1
     }
-    if (cost <= 60000 && cost <= 69999) {
-        return weekSub[9]
+    if (cost <= 60000 || cost <= 69999) {
+        return 0.9
     }
-    if (cost <= 70000 && cost <= 79999) {
-        return weekSub[10]
+    if (cost <= 70000 || cost <= 79999) {
+        return 0.9
     }
-    if (cost <= 80000 && cost <= 89999) {
-        return weekSub[11]
+    if (cost <= 80000 || cost <= 89999) {
+        return 0.8
     }
-    if (cost <= 90000 && cost <= 99999) {
-        return weekSub[12]
+    if (cost <= 90000 || cost <= 99999) {
+        return 0.8
     }
-    if (cost <= 100000 && cost <= 499999) {
-        return weekSub[13]
+    if (cost <= 100000 || cost <= 499999) {
+        return 0.8
     }
-    if (cost <= 500000 && cost <= 999999) {
-        return weekSub[14]
+    if (cost <= 500000 || cost <= 999999) {
+        return 0.3
     }
     if (cost <= 1000000) {
-        return weekSub[15]
+        return 0.2
     }
 }
 
 // TODO
 function getCostMonth(cost) {
-    if (cost <= 1000 && cost <= 2999) {
-        return monthSub[1]
+    if (cost <= 1000 || cost <= 2999) {
+        return 1.5
     }
-    if (cost <= 3000 && cost <= 4999) {
-        return monthSub[2]
+    if (cost <= 3000 || cost <= 4999) {
+        return 1.4
     }
-    if (cost <= 5000 && cost <= 9999) {
-        return monthSub[3]
+    if (cost <= 5000 || cost <= 9999) {
+        return 1.3
     }
-    if (cost <= 10000 && cost <= 19999) {
-        return monthSub[4]
+    if (cost <= 10000 || cost <= 19999) {
+        return 1.2
     }
-    if (cost <= 20000 && cost <= 29999) {
-        return monthSub[5]
+    if (cost <= 20000 || cost <= 29999) {
+        return 1.1
     }
-    if (cost <= 30000 && cost <= 39999) {
-        return monthSub[6]
+    if (cost <= 30000 || cost <= 39999) {
+        return 1
     }
-    if (cost <= 40000 && cost <= 49999) {
-        return monthSub[7]
+    if (cost <= 40000 || cost <= 49999) {
+        return 0.9
     }
-    if (cost <= 50000 && cost <= 59999) {
-        return monthSub[8]
+    if (cost <= 50000 || cost <= 59999) {
+        return 0.9
     }
-    if (cost <= 60000 && cost <= 69999) {
-        return monthSub[9]
+    if (cost <= 60000 || cost <= 69999) {
+        return 0.8
     }
-    if (cost <= 70000 && cost <= 79999) {
-        return monthSub[10]
+    if (cost <= 70000 || cost <= 79999) {
+        return 0.8
     }
-    if (cost <= 80000 && cost <= 89999) {
-        return monthSub[11]
+    if (cost <= 80000 || cost <= 89999) {
+        return 0.8
     }
-    if (cost <= 90000 && cost <= 99999) {
-        return monthSub[12]
+    if (cost <= 90000 || cost <= 99999) {
+        return 0.8
     }
-    if (cost <= 100000 && cost <= 499999) {
-        return monthSub[13]
+    if (cost <= 100000 || cost <= 499999) {
+        return 0.3
     }
-    if (cost <= 500000 && cost <= 999999) {
-        return monthSub[14]
+    if (cost <= 500000 || cost <= 999999) {
+        return 0.2
     }
     if (cost <= 1000000) {
-        return monthSub[15]
+        return 0.2
     }
 }
 
 // TODO
 function getOneTimeCost(cost) {
-    if (cost <= 1000 && cost <= 2999) {
-        return oneTimeSub[1]
+    if (cost <= 1000 || cost <= 2999) {
+        return 1.5
     }
-    if (cost <= 3000 && cost <= 4999) {
-        return oneTimeSub[2]
+    if (cost <= 3000 || cost <= 4999) {
+        return 1.4
     }
-    if (cost <= 5000 && cost <= 9999) {
-        return oneTimeSub[3]
+    if (cost <= 5000 || cost <= 9999) {
+        return 1.3
     }
-    if (cost <= 10000 && cost <= 19999) {
-        return oneTimeSub[4]
+    if (cost <= 10000 || cost <= 19999) {
+        return 1.2
     }
-    if (cost <= 20000 && cost <= 29999) {
-        return oneTimeSub[5]
+    if (cost <= 20000 || cost <= 29999) {
+        return 1
     }
-    if (cost <= 30000 && cost <= 39999) {
-        return oneTimeSub[6]
+    if (cost <= 30000 || cost <= 39999) {
+        return 1
     }
-    if (cost <= 40000 && cost <= 49999) {
-        return oneTimeSub[7]
+    if (cost <= 40000 || cost <= 49999) {
+        return 0.9
     }
-    if (cost <= 50000 && cost <= 59999) {
-        return oneTimeSub[8]
+    if (cost <= 50000 || cost <= 59999) {
+        return 0.8
     }
-    if (cost <= 60000 && cost <= 69999) {
-        return oneTimeSub[9]
+    if (cost <= 60000 || cost <= 69999) {
+        return 0.7
     }
-    if (cost <= 70000 && cost <= 79999) {
-        return oneTimeSub[10]
+    if (cost <= 70000 || cost <= 79999) {
+        return 0.6
     }
-    if (cost <= 80000 && cost <= 89999) {
-        return oneTimeSub[11]
+    if (cost <= 80000 || cost <= 89999) {
+        return 0.6
     }
-    if (cost <= 90000 && cost <= 99999) {
-        return oneTimeSub[12]
+    if (cost <= 90000 || cost <= 99999) {
+        return 0.6
     }
-    if (cost <= 100000 && cost <= 499999) {
-        return oneTimeSub[13]
+    if (cost <= 100000 || cost <= 499999) {
+        return 0.6
     }
-    if (cost <= 500000 && cost <= 999999) {
-        return oneTimeSub[14]
+    if (cost <= 500000 || cost <= 999999) {
+        return 0.3
     }
     if (cost <= 1000000) {
-        return oneTimeSub[15]
+        return 0.2
     }
 }
 
+function getPhisCostWeek(cost) {
+    if (cost <= 1000 || cost <= 2999) {
+        return 1.5
+    }
+    if (cost <= 3000 || cost <= 4999) {
+        return 1.5
+    }
+    if (cost <= 5000 || cost <= 9999) {
+        return 1.4
+    }
+    if (cost <= 10000 || cost <= 19999) {
+        return 1.3
+    }
+    if (cost <= 20000 || cost <= 29999) {
+        return 1.2
+    }
+    if (cost <= 30000 || cost <= 39999) {
+        return 1.1
+    }
+    if (cost <= 40000 || cost <= 49999) {
+        return 1
+    }
+    if (cost <= 50000 || cost <= 59999) {
+        return 0.9
+    }
+    if (cost <= 60000 || cost <= 69999) {
+        return 0.9
+    }
+    if (cost <= 70000 || cost <= 79999) {
+        return 0.8
+    }
+    if (cost <= 80000 || cost <= 89999) {
+        return 0.8
+    }
+    if (cost <= 90000 || cost <= 99999) {
+        return 0.8
+    }
+    if (cost <= 100000 || cost <= 499999) {
+        return 0.8
+    }
+    if (cost <= 500000 || cost <= 999999) {
+        return 0.3
+    }
+    if (cost <= 1000000) {
+        return 0.2
+    }
+}
+
+function getPhisCostMonth(cost) {
+    if (cost <= 1000 || cost <= 2999) {
+        return 1.3
+    }
+    if (cost <= 3000 || cost <= 4999) {
+        return 1.3
+    }
+    if (cost <= 5000 || cost <= 9999) {
+        return 1.2
+    }
+    if (cost <= 10000 || cost <= 19999) {
+        return 1.1
+    }
+    if (cost <= 20000 || cost <= 29999) {
+        return 1
+    }
+    if (cost <= 30000 || cost <= 39999) {
+        return 1
+    }
+    if (cost <= 40000 || cost <= 49999) {
+        return 0.9
+    }
+    if (cost <= 50000 || cost <= 59999) {
+        return 0.8
+    }
+    if (cost <= 60000 || cost <= 69999) {
+        return 0.7
+    }
+    if (cost <= 70000 || cost <= 79999) {
+        return 0.7
+    }
+    if (cost <= 80000 || cost <= 89999) {
+        return 0.7
+    }
+    if (cost <= 90000 || cost <= 99999) {
+        return 0.7
+    }
+    if (cost <= 100000 || cost <= 499999) {
+        return 0.7
+    }
+    if (cost <= 500000 || cost <= 999999) {
+        return 0.3
+    }
+    if (cost <= 1000000) {
+        return 0.2
+    }
+}
+
+function getPhisCostOneTime(cost) {
+    if (cost <= 1000 || cost <= 2999) {
+        return 0.4
+    }
+    if (cost <= 3000 || cost <= 4999) {
+        return 0.4
+    }
+    if (cost <= 5000 || cost <= 9999) {
+        return 0.4
+    }
+    if (cost <= 10000 || cost <= 19999) {
+        return 0.4
+    }
+    if (cost <= 20000 || cost <= 29999) {
+        return 0.4
+    }
+    if (cost <= 30000 || cost <= 39999) {
+        return 0.4
+    }
+    if (cost <= 40000 || cost <= 49999) {
+        return 0.4
+    }
+    if (cost <= 50000 || cost <= 59999) {
+        return 0.4
+    }
+    if (cost <= 60000 || cost <= 69999) {
+        return 0.4
+    }
+    if (cost <= 70000 || cost <= 79999) {
+        return 0.3
+    }
+    if (cost <= 80000 || cost <= 89999) {
+        return 0.3
+    }
+    if (cost <= 90000 || cost <= 99999) {
+        return 0.3
+    }
+    if (cost <= 100000 || cost <= 499999) {
+        return 0.3
+    }
+    if (cost <= 500000 || cost <= 999999) {
+        return 0.3
+    }
+    if (cost <= 1000000) {
+        return 0.2
+    }
+}
 // 1)Кол-во контактов в день = кол-во операторов * кол-во звонков в день
 function dayContacts(operators, dayCalls) {
    return operators * dayCalls;
@@ -169,18 +302,12 @@ function monthContacts(operators, dayCalls, schedule) {
     return operators * dayCalls * schedule;
 }
 
-// 3)Стоимость недельной подписки = кол-во контактов в неделю * стоимость
-
-function weekSubCost(operators, dayCalls, schedule, contactsAll) {
-    return weekContacts(operators, dayCalls, schedule) * getCostWeek(contactsAll);
-}
-
-function monthSubCost(operators, dayCalls, schedule, contactsAll) {
-    return weekContacts(operators, dayCalls, schedule) * getCostMonth(contactsAll);
-}
-
 function oneTimeSubCost(contactsAll) {
     return contactsAll * getOneTimeCost(contactsAll);
+}
+
+function oneTimeSubCostFis(contactsAll) {
+    return contactsAll * getPhisCostOneTime(contactsAll);
 }
 
 function remove() {
@@ -193,6 +320,8 @@ function remove() {
     document.getElementById("sumMonth").innerText="";
     document.getElementById('operators').value="";
     document.getElementById('phoneCalls').value="";
+    document.getElementById("costWeek").innerText="";
+    document.getElementById("costMonth").innerText="";
 }
 
 function show() {
@@ -204,6 +333,17 @@ function show() {
     document.getElementById("sumDay").innerText="";
     document.getElementById("sumWeek").innerText="";
     document.getElementById("sumMonth").innerText="";
+    document.getElementById("costWeek").innerText="";
+    document.getElementById("costMonth").innerText="";
+}
+
+function clearResult() {
+    document.getElementById("sumDay").innerText="";
+    document.getElementById("sumWeek").innerText="";
+    document.getElementById("sumMonth").innerText="";
+    document.getElementById("costWeek").innerText="";
+    document.getElementById("costMonth").innerText="";
+    document.getElementById("sumBase").innerText="";
 }
 
 function getPricing() {
@@ -214,13 +354,25 @@ function getPricing() {
 
     const operators = document.getElementById('operators').valueAsNumber; // Количество операторов:
     const dayCalls = document.getElementById('phoneCalls').valueAsNumber; // Количество звонков в день:
+    const week = weekContacts(operators, dayCalls, scheduleWeek);
+    const month = monthContacts(operators, dayCalls, scheduleMonth);
 
     if (isNaN(operators) || isNaN(dayCalls)) {
-        document.getElementById("sumDay").innerText=`Стоимость базы: ${oneTimeSubCost(totalContacts)}`;
+        if (business === 'false') {
+            document.getElementById("sumBase").innerText=`Стоимость базы: ${oneTimeSubCostFis(totalContacts)}`;
+        } else {
+            document.getElementById("sumBase").innerText=`Стоимость базы: ${oneTimeSubCost(totalContacts)}`;
+        }
     } else {
         document.getElementById("sumDay").innerText=`Контактов в день: ${dayContacts(operators, dayCalls)}`;
         document.getElementById("sumWeek").innerText=`Контактов в неделю: ${weekContacts(operators, dayCalls, scheduleWeek)}`;
         document.getElementById("sumMonth").innerText=`Контактов в месяц: ${monthContacts(operators, dayCalls, scheduleMonth)}`;
+        document.getElementById("costWeek").innerText=`Цена в неделю: ${weekContacts(operators, dayCalls, scheduleWeek) * getCostWeek(week)}`;
+        document.getElementById("costMonth").innerText=`Цена в месяц: ${Math.round(monthContacts(operators, dayCalls, scheduleMonth) * getCostMonth(month))}`;
+        if (business === 'true') {
+            document.getElementById("costWeek").innerText=`Цена в неделю: ${Math.round(weekContacts(operators, dayCalls, scheduleWeek) * getPhisCostWeek(week))}`;
+            document.getElementById("costMonth").innerText=`Цена в месяц: ${Math.round(monthContacts(operators, dayCalls, scheduleMonth) * getPhisCostMonth(month))}`;
+        }
     }
 }
 
